14192d6a703d4e1c9a7de1af457e8e9e76ead4a0
01mohitjangid <01mohitjangid@gmail.com>
2026-01-14 20:36:21 +0530

    repush

--- src/libs/db.py
+++ src/libs/db.py
@@ -1,15 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class Database:
-    def __init__(self, db_url=None):
-        # Use environment variable or default to local SQLite
-        if db_url is None:
-            db_url = os.getenv('DATABASE_URL', 'sqlite:///thanos.db')
-        self.engine = create_engine(db_url)
-        self.Session = sessionmaker(bind=self.engine)
-
+    
     def get_session(self):
         """Create and return a new database session"""
         return self.Session()
